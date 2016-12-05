5eef75c6d52bff9379f296b38eff19b38a9f0f25
pedroivo96 <180596pedro>
2016-12-05 17:00:17 -0300

    definiçao dos pacotes.

---
+++ app/src/main/java/br/ufpi/alugapp/MainActivity.java
@@ -0,0 +1,9 @@
+package br.ufpi.alugapp;
+
+import android.app.Activity;
+
+/**
+ * Created by wellyson on 05/12/16.
+ */
+public class MainActivity extends Activity {
+}
---
+++ app/src/main/java/br/ufpi/alugapp/repositorio/Firebase.java
@@ -0,0 +1,8 @@
+package br.ufpi.alugapp.repositorio;
+
+/**
+ * Created by wellyson on 05/12/16.
+ */
+
+public class Firebase {
+}
