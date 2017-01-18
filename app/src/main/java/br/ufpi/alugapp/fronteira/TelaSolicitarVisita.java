fb2c6865865f2ff5efa2400abd03d971397328ab
pedroivo96 <soaresbarbosapedroivo@gmail.com>
2017-01-18 12:15:38 -0300

    alteracao TelaSolicitarVisita

--- app/src/main/java/br/ufpi/alugapp/fronteira/TelaSolicitarVisita.java
+++ app/src/main/java/br/ufpi/alugapp/fronteira/TelaSolicitarVisita.java
@@ -9,33 +9,48 @@ import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import br.ufpi.alugapp.R;
+import br.ufpi.alugapp.controle.Fachada;
 
 public class TelaSolicitarVisita extends AppCompatActivity {
+
+    private Fachada controles;
+    private int IDImovel;
+    private int IDCliente;
+    private int IDCorretor;
+    private Date data;
+    private TextView tIDImovel;
+    private EditText edtMensagem;
+    private CalendarView calendario
+    private Spinner spHoras;
+    private Spinner spMinutos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_solicitar_visita);
 
-        int IDimovel = getIntent().getIntExtra("IDImovel", 0);
-        TextView tIDImovel = (TextView) findViewById(R.id.tIDImovel);
-        tIDImovel.setText(Integer.toString(IDimovel));
-    }
+        controles = new Fachada(this);
 
-    public void solicitarVisita(View view){
+        IDImovel = getIntent().getIntExtra("IDImovel", 0);
+        IDCliente = getIntent().getIntExtra("IDCliente", 0);
+        IDCorretor = getIntent().getIntExtra("IDCorretor", 0);
 
-        EditText edtMensagem = (EditText) findViewById(R.id.edtMensagem);
-        CalendarView calendario = (CalendarView) findViewById(R.id.calendar) ;
-        Spinner spHoras = (Spinner) findViewById(R.id.spHoras);
-        Spinner spMinutos = (Spinner) findViewById(R.id.spMinutos);
+        tIDImovel = (TextView) findViewById(R.id.tIDImovel);
+        tIDImovel.setText(Integer.toString(IDImovel));
 
-        final Date data = new Date();
+        edtMensagem = (EditText) findViewById(R.id.edtMensagem);
+        calendario = (CalendarView) findViewById(R.id.calendar) ;
+        spHoras = (Spinner) findViewById(R.id.spHoras);
+        spMinutos = (Spinner) findViewById(R.id.spMinutos);
+
+        data = new Date();
 
         final String[] horas = new String[24];
         final String[] minutos = new String[60];
@@ -79,6 +94,24 @@ public class TelaSolicitarVisita extends AppCompatActivity {
                 data.setMinutes(minuto);
             }
         });
+    }
+
+    public void solicitarVisita(View view){
+
+        if(edtMensagem.getText().toString().isEmpty() == false){
+
+            if(controles.controladorVisitas.solicitarVisita(IDImovel , IDCliente , IDCorretor , data , edtMensagem.getText().toString())){
+                Toast.makeText(this, "Solicitação Enviada com Sucesso", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this, "Erro ! Tente Novamente", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+        else{
+            Toast.makeText(this, "Entrada Inválida", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
